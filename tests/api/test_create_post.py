9fb9f441aa1467444591dde3ad10026a239e9a9d
your <robelavraham@gmail.com>
2025-07-13 15:37:33 +0300

    Fix: use relative path for data directory

--- tests/api/test_create_post.py
+++ tests/api/test_create_post.py
@@ -3,6 +3,8 @@ from utils.http_client import post
 from utils.json_loader import load_json
 from utils.json_loader import get_all_json_files
 import pytest
+import os
+from pathlib import Path
 
 #def test_create_post():
     #url = "https://jsonplaceholder.typicode.com/posts"
@@ -13,8 +15,10 @@ import pytest
     #assert data["body"] == response.json()["body"], f"Expected {response.json()['body']}, got {response.json()['body']}"
     #assert data["userId"] == response.json()["userId"], f"Expected {response.json()['userId']}, got {response.json()['userId']}"
     #assert "id" in response.json()
+DATA_DIR = Path(__file__).resolve().parent.parent.parent / "data" / "create_post"
 
-@pytest.mark.parametrize("json_file", get_all_json_files(r"C:\Users\avrah\PycharmProjects1\PythonProject\Api_ui_practice\data\create_post"))
+
+@pytest.mark.parametrize("json_file", get_all_json_files(str(DATA_DIR)))
 def test_create_post_with_multiple_dataload(json_file):
     url = "https://jsonplaceholder.typicode.com/posts"
     data = load_json(json_file)
